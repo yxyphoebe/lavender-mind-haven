761524e422af67f06fb05c0141dc5007fa99257c
Phoebe Yu <phoebe@Phoebes-MacBook-Air.local>
2025-06-03 10:31:05 -0700

    Add iOS platform and build files

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,30 +1,9 @@
-
-import { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'app.lovable.c00eb517aca2490ca1b18cca6472e0a2',
+  appId: 'com.mindfulai.app',
   appName: 'Mindful AI',
-  webDir: 'dist',
-  server: {
-    url: 'https://c00eb517-aca2-490c-a1b1-8cca6472e0a2.lovableproject.com?forceHideBadge=true',
-    cleartext: true
-  },
-  plugins: {
-    SplashScreen: {
-      launchShowDuration: 3000,
-      launchAutoHide: true,
-      backgroundColor: '#fcfbff',
-      androidSplashResourceName: 'splash',
-      androidScaleType: 'CENTER_CROP',
-      showSpinner: false,
-      splashFullScreen: true,
-      splashImmersive: true,
-    },
-    StatusBar: {
-      style: 'DARK',
-      backgroundColor: '#fcfbff',
-    },
-  },
+  webDir: 'dist'
 };
 
 export default config;
